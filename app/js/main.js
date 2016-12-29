b43876de64a6bb46d3d0b4c77308d91ebf248770
slabitskiy <slabitskiy.igor@yandex.ua>
2016-12-29 02:50:09 +0200

    finished landing desktop version

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-	
+	var owl = $('#owl');
+	owl.owlCarousel({
+	    loop:true,
+	    margin:0,
+	   items: 1
+	});
+
+	$('.nav__button').on('click', function(event) {
+		event.preventDefault();
+		if ($(this).hasClass('nav__button-left')){
+			owl.trigger('prev.owl.carousel');
+			return;
+		}
+		owl.trigger('next.owl.carousel');
+	});
 });
